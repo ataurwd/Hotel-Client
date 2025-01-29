688dfa9f1aa2c937daff1a0ab2f67e77af0b0674
ataur rahman <ataurrahmanbro@gmail.com>
2025-01-30 00:06:20 +0600

    update

--- src/components/BistroBoss.jsx
+++ src/components/BistroBoss.jsx
@@ -12,7 +12,7 @@ const BistroBoss = () => {
       <div className=" text-center flex flex-col items-center justify-center space-y-5 h-[300px] bg-white absolute top-[20%] bottom-[20%] left-[10%] right-[10%] rounded-md">
         <h1 className="text-black text-4xl font-bold"></h1>
         <p className="w-1/2">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          Lorem ipsum dolor sit amet  adipisicing elit.
           Necessitatibus, libero accusamus laborum deserunt ratione dolor
           officiis praesentium! Deserunt magni aperiam dolor eius dolore at,
           nihil iusto ducimus incidunt quibusdam nemo.
